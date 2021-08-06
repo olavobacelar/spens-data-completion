6b4bb10000fedee20a9354e194c0f1bee8727cce
olavobacelar <olavobacelar@gmail.com>
2021-08-06 18:54:34 +0100

    Update model.py

--- src/model.py
+++ src/model.py
@@ -4,20 +4,23 @@ import torch.nn.functional as F
 
 N_CHARACTERS = 26
 
-def encoder_block(n_layers, in_channels, out_channels, kernel_size=3, out=False, groups=1):
+def encoder_block(n_layers, in_channels, out_channels, kernel_size=3, use_batchnorm=False, use_softplus=True, beta=1.):
     "n_layers is the number of blocks of Conv2d + BackNorm2d + ReLU"
     padding = kernel_size // 2
     block = []
     for layer_index in range(n_layers):
         if layer_index > 0:
             in_channels = out_channels
-        block.append(nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, groups=groups))
-        block.append(nn.BatchNorm2d(out_channels))
-        if out is False or layer_index != n_layers-1:
+        block.append(nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding))
+        if use_batchnorm:
+            block.append(nn.BatchNorm2d(out_channels))
+        if use_softplus:
+            block.append(nn.Softplus(beta))
+        else:
             block.append(nn.ReLU())
     return nn.Sequential(*block)
 
-def decoder_block(n_layers, in_channels, out_channels, kernel_size=3, out=False, groups=1):
+def decoder_block(n_layers, in_channels, out_channels, kernel_size=3, out=False, use_batchnorm=False, use_softplus=True, beta=1.):
     "n_layers is the number of blocks of Conv2d + BackNorm2d + ReLU"
     padding = kernel_size // 2
     block = []
@@ -25,155 +28,139 @@ def decoder_block(n_layers, in_channels, out_channels, kernel_size=3, out=False,
     for layer_index in range(n_layers):
         if layer_index == n_layers-1:
             out_channels_ = out_channels 
-        block.append(nn.ConvTranspose2d(in_channels, out_channels_, kernel_size=kernel_size, padding=padding, groups=groups))
-        block.append(nn.BatchNorm2d(out_channels_))
+        block.append(nn.ConvTranspose2d(in_channels, out_channels_, kernel_size=kernel_size, padding=padding))
+        if use_batchnorm:
+            block.append(nn.BatchNorm2d(out_channels_))
         if out is False or layer_index != n_layers-1:
-            block.append(nn.ReLU())
+            if use_softplus:
+                block.append(nn.Softplus(beta))
+            else:
+                block.append(nn.ReLU())
     return nn.Sequential(*block)
-	
 
+@dataclass(frozen=True)
+class EnergyNetConfig:
+    '''To configure an Energy Network architecture, set those parameters below.
 
-# retirar bias no conv2d??
-# https://github.com/vdumoulin/conv_arithmetic/blob/master/README.md
-#Include visualizations of the feature maps!
-# semelhante a unet e a segnet
-# este codigo é fixe:https://github.com/milesial/Pytorch-UNet/blob/master/unet/unet_parts.py
-# o que dizer sobre o receptive field?
-# usar o softmax por cima de aplicação linear é equivalente a multinomial logistic regression? por mais bem escrito
-# Podemos por ordereddict para ficar com os nomes das layers
+    It's not exactly necessary, but is helpful to have the configuration
+    as a dataclass (Google Colab still doesn't type check anything though.)
+    '''
 
-# n_channels_per_character = {
-#     'encoder_1': 2,
-#     'encoder_2': 4,
-#     'encoder_3': 4, 
-#     'encoder_4': 6,
-#     }
+    n_channels_conv: Tuple[int]
+    n_layers_per_conv_block: Tuple[int]
+    kernel_sizes: Tuple[int]
+    n_linear_features: Tuple[int]
+    dropout_probs: Tuple[float]
+    use_batchnorm: bool = False
+    beta: float = 1.
+    last_softplus: bool = False
+    use_softplus: bool = True
 
+    def __post_init__(self):
+        # With Google Colab we can't easily use type hints, so we add this
+        if not (isinstance(self.n_channels_conv, tuple) and isinstance(self.n_layers_per_conv_block, tuple) and
+            isinstance(self.kernel_sizes, tuple) and isinstance(self.n_linear_features, tuple) and
+            isinstance(self.dropout_probs, tuple) and isinstance(self.use_batchnorm, bool) and
+            isinstance(self.beta, float) and isinstance(self.last_softplus, bool) and
+            isinstance(self.use_softplus, bool)):
+            raise TypeError('The parameters must be of the given types!')
+        if len(self.n_channels_conv) != len(self.n_layers_per_conv_block) or \
+            len(self.n_layers_per_conv_block) != len(self.kernel_sizes):
+            raise ValueError('n_channels_conv, n_layers_per_conv_block, and '
+                              'kernel_sizes must have the same length!')
+        if len(self.n_linear_features) != len(self.dropout_probs):
+            raise ValueError('n_linear_features and dropout_probs must have the same length!')
 
-# Agora já não é necessário ter multiplo de N_CHARACTERS!
-n_channels_per_character = {
-    'encoder_1': 2,
-    'encoder_2': 2,
-    'encoder_3': 4, 
-    'encoder_4': 4,
-    }
+    def __iter__(self):
+        # Dataclass doesn't unpack by default
+        # I could have written that:
+        
+        # return iter([self.n_channels_conv, self.kernel_sizes, self.n_features_fc,
+        #             self.beta, self.use_softplus, self.last_softplus])
+        return iter(astuple(self))
 
-n_channels_encoder_1 = N_CHARACTERS*n_channels_per_character['encoder_1']
-n_channels_encoder_2 = N_CHARACTERS*n_channels_per_character['encoder_2']
-n_channels_encoder_3 = N_CHARACTERS*n_channels_per_character['encoder_3']
-n_channels_encoder_4 = N_CHARACTERS*n_channels_per_character['encoder_4']
-
-n_channels_decoder_1 = n_channels_encoder_1
-n_channels_decoder_2 = n_channels_encoder_2
-n_channels_decoder_3 = n_channels_encoder_3
-n_channels_decoder_4 = n_channels_encoder_4
-
-n_channels_mixer_1 = n_channels_encoder_4*3*3 # 3 is the size in pixels of ?? after the encoding
-n_channels_mixer_2 = 1024
-
-n_channels_fc_1 = n_channels_encoder_4*3*3
-n_channels_fc_2 = 1024
-n_channels_fc_3 = 256
-n_channels_fc_4 = 56
-
-# começar a usar parâmetros partilhados nas convoluções a partir de certo momento?
+    def print_training_repr(self):
+        print(f'EnergyNet Config: n_channels_conv: {self.n_channels_conv} • n_layers_per_conv_block: {self.n_layers_per_conv_block} • '
+              f'kernel_sizes: {self.kernel_sizes}\n' + ' '*18 + f'n_linear_features: {self.n_linear_features} • '
+              f'dropout_probs: {self.dropout_probs} • beta: {self.beta}')
 
 class EnergyNet(nn.Module):
-    def __init__(self, inference_mode=False):
-        super(EnergyNet, self).__init__()
-        self._saved_encoding = None
-
-        #Encoders
-        self.encode_conv_1 = encoder_block(2, N_CHARACTERS, n_channels_encoder_1, kernel_size=5, groups=1)
-        self.encode_conv_2 = encoder_block(2, n_channels_encoder_1, n_channels_encoder_2, kernel_size=3, groups=1)
-        self.encode_conv_3 = encoder_block(2, n_channels_encoder_2, n_channels_encoder_3, kernel_size=3, groups=1)
-        self.encode_conv_4 = encoder_block(3, n_channels_encoder_3, n_channels_encoder_4, kernel_size=3, groups=1)
-
-        # Mixers (this and the next decoding ones are just for the init_net)
-        self.mix = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(in_features=n_channels_mixer_1, out_features=n_channels_mixer_2),
-            # nn.Dropout(0.2),
-            nn.ReLU(),
-            nn.Linear(in_features=n_channels_mixer_2, out_features=n_channels_mixer_1),
-            # nn.Dropout(0.3),
-            nn.ReLU(),
-        )
-
-        
-        # Decoders
-        self.decode_conv_4 = decoder_block(3, n_channels_decoder_4, n_channels_decoder_3, kernel_size=3, groups=1)
-        self.decode_conv_3 = decoder_block(2, n_channels_decoder_3, n_channels_decoder_2, kernel_size=3, groups=1)
-        self.decode_conv_2 = decoder_block(2, n_channels_decoder_2, n_channels_decoder_1, kernel_size=3, groups=1)
-        self.decode_conv_1 = decoder_block(2, n_channels_decoder_1, N_CHARACTERS, kernel_size=5, groups=1, out=True)
-
-        # this is the fully connected layer in the end of the usual energy_net
-        self.fc = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(in_features=n_channels_fc_1, out_features=n_channels_fc_2),
-            # nn.Dropout(0.2),
-            # nn.ReLU(),
-            nn.Softplus(beta=25, threshold=20),
-            nn.Linear(in_features=n_channels_fc_2, out_features=n_channels_fc_3),
-            # nn.Dropout(0.2),
-            # nn.ReLU(),
-            nn.Softplus(beta=25, threshold=20),
-            nn.Linear(in_features=n_channels_fc_3, out_features=n_channels_fc_4),
-            # nn.Dropout(0.3),
-            # nn.ReLU(),
-            nn.Softplus(beta=25, threshold=20),
-            # It doesn't apply bias since it's not envolved in the differentiation of the energy in order to y
-            nn.Linear(in_features=n_channels_fc_4, out_features=1, bias=False),  
-            nn.Softplus(beta=25, threshold=20),                 
-        )
-
-    def forward(self, x, mode=None, save_encoding=False):
-        if mode is None:
-            mode = self.mode
-        else:
-            self.mode = mode
-
-        # if self.mode == 'init_energy':
-        #     return self.fc(self.saved_encoding)
-
-        # Encode
-        x = self.encode_conv_1(x)
-        x, indices_1 = F.max_pool2d(x, kernel_size=2, stride=2, return_indices=True)
-        x = self.encode_conv_2(x)
-        x, indices_2 = F.max_pool2d(x, kernel_size=2, stride=2, return_indices=True)
-        x = self.encode_conv_3(x)
-        x, indices_3 = F.max_pool2d(x, kernel_size=2, stride=2, return_indices=True)
-        x = self.encode_conv_4(x)
-        x, indices_4 = F.max_pool2d(x, kernel_size=2, stride=2, return_indices=True)
-        # if save_encoding:
-        #     # Usar o clone, e o detach?
-        #     self.saved_encoding = x.clone()
-        # else:
-        #     self.saved_encoding = None
-
-        if self.mode == 'init':
-            shape_after_encoding = x.shape
-            x = self.mix(x)
-            
-            # Decode (include the dimensions as parameter to give good result? https://github.com/say4n/pytorch-segnet/blob/f7738c6bce384b54fcbb3fe8aff02736d6ec2285/src/model.py#L333)
-            x = x.view(shape_after_encoding)
-            x = F.max_unpool2d(x, indices_4, kernel_size=2, stride=2)
-            x = self.decode_conv_4(x)
-            x = F.max_unpool2d(x, indices_3, kernel_size=2, stride=2)
-            x = self.decode_conv_3(x)
-            x = F.max_unpool2d(x, indices_2, kernel_size=2, stride=2)
-            x = self.decode_conv_2(x)
-            x = F.max_unpool2d(x, indices_1, kernel_size=2, stride=2)
-            x = self.decode_conv_1(x)
-            self.output = x
-            # self.output = torch.sigmoid(x)
-        elif self.mode == 'usual':
-            # vou precisar de self.encoded_input para mais alguma coisa?
-            self.output = self.fc(x)
-        else:
-            raise Exception('Introduce a valid mode!')
+    """Energy Net accepting a configuration object for defining the architecture
     
-        return self.output
+    TO DO: Maybe generalize for the other energy nets to be used."""
+
+    # transformar todas as relus em softplus!
+    def __init__(self, config, architecture='usual', beta_is_parameter=False):
+        super(EnergyNet, self).__init__()
+        
+        if not isinstance(config, EnergyNetConfig):
+            raise TypeError('One must insert a valid EnergyNetConfig!')
+
+        self.architecture = architecture
+        n_channels_conv, n_layers_per_conv_block, kernel_sizes, n_linear_features, \
+        dropout_probs, use_batchnorm, beta, last_softplus, use_softplus = config
+        n_conv_blocks = int(len(n_layers_per_conv_block))
+
+        # self.beta = torch.tensor(beta, device=device)
+        # if beta_is_parameter:
+        #     self.beta = torch.nn.Parameter(self.beta)
+        
+        softplus = torch.nn.Softplus(beta)
+
+        n_channels_conv = [N_LETTERS] + list(n_channels_conv)
+
+        conv_modules = OrderedDict()
+        # self.conv = nn.ModuleList()
+        for i, (n_layers, kernel_size) in enumerate(zip(n_layers_per_conv_block, kernel_sizes)):
+            # self.conv.append(encoder_block(n_layers, n_channels_conv[i], n_channels_conv[i+1], kernel_size, use_softplus, beta))
+            conv_modules['encode_conv_' + str(i+1)] = encoder_block(n_layers, n_channels_conv[i], n_channels_conv[i+1],
+                                                                    kernel_size, use_batchnorm, use_softplus, beta)
+        self.conv = nn.Sequential(conv_modules)
+
+        # This will compute the number of pixels in the last feature maps after all the cnn transformations
+        # n_conv_blocks is the number of blocks, each of which reduces the size of the 
+        # feature maps by a half
+        side_size_last_feature_map = int(SIDE_SIZE_PIXELS / (2**n_conv_blocks))
+        n_pixels_last_feature_map = int(side_size_last_feature_map**2)
+
+        n_linear_features = [n_channels_conv[-1]*n_pixels_last_feature_map] + \
+                            list(n_linear_features)
+        self.fc = nn.ModuleList()
+        self.fc.append(nn.Flatten())
+        for layer_idx, dropout_prob in enumerate(dropout_probs):
+            self.fc.append(nn.Linear(in_features=n_linear_features[layer_idx], out_features=n_linear_features[layer_idx+1]))
+            self.fc.append(nn.Dropout(dropout_prob))
+            self.fc.append(softplus)
+        # It doesn't apply bias since it's not envolved in the differentiation of the energy in order to y
+        self.fc.append(nn.Linear(in_features=n_linear_features[-1], out_features=1, bias=False))
+
+        if last_softplus:
+            self.fc.append(softplus)
+
+    def forward(self, x, mode='init'):  # apagar mode
+
+        if self.architecture == 'usual':
+            return_indices = False
+
+        for block_idx in range(len(self.conv)):
+            x = self.conv[block_idx](x)
+            x = F.max_pool2d(x, kernel_size=2, stride=2, return_indices=return_indices)
+    
+        for op_idx in range(len(self.fc)):
+            x = self.fc[op_idx](x)
+
+        return x
+
+
+# Teste da EnergyNet
+
+# energy_net_config_A = EnergyNetConfig(n_channels_conv = (120, 140, 180, 220),
+#                           n_layers_per_conv_block = (2, 2, 3, 3),
+#                           kernel_sizes = (5, 5, 3, 3),
+#                           n_linear_features = (200, 80),
+#                           dropout_probs = (0.3, 0.5))
+
+# energy_net = EnergyNet(energy_net_config_A).to(device)
 
     # @property
     # def saved_encoding(self):
