3a85ff766df77bd64cad73bf10756bb9000e3d8e
olavobacelar <olavobacelar@gmail.com>
2021-08-16 18:21:03 +0100

    Update spens.py

--- src/spens.py
+++ src/spens.py
@@ -12,7 +12,7 @@ import torch.optim as optim
 from metrics_losses import init_running_metrics, update_running_metrics, compute_final_metrics
 from dataset import FontsDataset
 from model import EnergyNet
-from utils import print_header_scores, current_time, current_time_repr, discretize, get_usual_energy_net_params, save_checkpoint, load_checkpoint, 
+from utils import print_header_scores, current_time, current_time_repr, discretize, get_usual_energy_net_params, save_checkpoint, load_checkpoint
 
 @dataclass
 class UnrollConfigNew:
